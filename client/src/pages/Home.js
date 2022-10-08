54d333be3f7d0d42731fce48a3642ce8e727ae92
goldminer127 <Goldenplaymc@yahoo.com>
2022-10-08 17:47:07 -0400

    Peach is on the cloud

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,4 @@
 import Container from "react-bootstrap/Container"
-import Peach from "./images/Peach.jpg"
 import Image from "react-bootstrap/Image"
 
 function Home()
@@ -10,7 +9,7 @@ function Home()
                 <h5>Media Library</h5>
             </Container>
                 <Container style={{display: 'flex'}}>
-                <Image width={300} height={300} src={Peach}/>
+                <Image width={300} height={300} src="https://peach418.s3.amazonaws.com/Peach.jpg"/>
                 <Container style={{padding: '.5rem', marginLeft: '.5rem'}}>
                     <h2>Peach Friend</h2>
                     <p>This lovely peach was picked from a house and had something special to show the people.</p>
