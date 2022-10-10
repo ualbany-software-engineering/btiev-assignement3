3bb2994d2e638710c914868a7340876158eac084
goldminer127 <Goldenplaymc@yahoo.com>
2022-10-09 20:30:06 -0400

    Replaced routes and added second page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-import Container from "react-bootstrap/Container"
-import Image from "react-bootstrap/Image"
+import Home from "./pages/Home.js"
+import HelloWorld from "./pages/HelloWorld.js"
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Container style={{textAlign: 'left'}}>
-      <Container style={{backgroundColor: 'lightgray', textAlign: 'left'}}>
-        <h5>Media Library</h5>
-      </Container>
-        <Container style={{display: 'flex'}}>
-        <Image width={300} height={300} src="https://peach418.s3.amazonaws.com/Peach.jpg"/>
-        <Container style={{padding: '.5rem', marginLeft: '.5rem'}}>
-            <h2>Peach Friend</h2>
-            <p>This lovely peach was picked from a house and had something special to show the people.</p>
-        </Container>
-      </Container>
-    </Container>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="" element={<HelloWorld/>}/>
+          <Route path="/profile" element={<Home/>}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
