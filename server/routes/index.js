c637993cb2bc522e5f5f809c8842983b414de39a
goldminer127 <Goldenplaymc@yahoo.com>
2022-10-11 20:27:59 -0400

    Changed submission to no react

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,9 @@
 var express = require('express');
-var path = require('path')
 var router = express.Router();
-var app = express()
 
 /* GET home page. */
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './build/', 'index.html'));
-});
-app.use('/', router);
-
-//note, unlike POST, parameters are visible to a user in browser address bar in GET request.
-router.get('/add/:firstNumber/and/:secondNumber', (req,res)=>{
-  console.log(req.params.firstNumber + req.params.secondNumber);
-  //Checkout console to see why parseInt is essential in this case.
-  let firstNo = parseInt(req.params.firstNumber),
-      secondNo = parseInt(req.params.secondNumber);
-  res.json({"Addition" : firstNo + secondNo});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
