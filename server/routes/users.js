e7a50767b26e1aa9444f98d935ba00f0a50aa4cd
goldminer127 <Goldenplaymc@yahoo.com>
2022-10-11 21:03:31 -0400

    Added endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//note, unlike POST, parameters are visible to a user in browser address bar in GET request.
+router.get('/add/:firstNumber/and/:secondNumber', (req,res)=>{
+  console.log(req.params.firstNumber + req.params.secondNumber);
+  //Checkout console to see why parseInt is essential in this case.
+  let firstNo = parseInt(req.params.firstNumber),
+      secondNo = parseInt(req.params.secondNumber);
+  res.json({"Addition" : firstNo + secondNo});
+});
+
 module.exports = router;
